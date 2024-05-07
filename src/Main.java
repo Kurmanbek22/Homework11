362c2251c62654ea4dd648b4d9cce1e763f43337
1 <a1@MacBook-Pro-1.local>
2024-05-08 01:35:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+Triangle triangle= new Triangle();
+triangle.a=12;
+triangle.b=23;
+triangle.c= 40;
+triangle.rezult();
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,8 @@
+public class Triangle {
+    int a;
+    int b;
+    int c;
+    public void rezult(){
+        System.out.println(a+b+c+"-uch burchtuktun aianty");
+    }
+}
